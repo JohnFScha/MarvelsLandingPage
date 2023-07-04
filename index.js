42fc75b8e997f6cd0bba91a1369f47103a690121
Juan Francisco Schallibaum <jfschallibaum@gmail.com>
2023-07-04 16:21:02 -0300

    fix: changed few features and added animations

--- index.js
+++ index.js
@@ -1,51 +1,50 @@
 // Function to make an AJAX request and load HTML content
 function loadPage(url, containerId) {
-  var xhttp = new XMLHttpRequest();
+  var xhttp = new XMLHttpRequest()
   xhttp.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
-      document.getElementById(containerId).innerHTML = this.responseText;
+      document.getElementById(containerId).innerHTML = this.responseText
     }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
+  }
+  xhttp.open("GET", url, true)
+  xhttp.send()
 }
 
 // Function to handle navigation and load the corresponding page
 function navigateTo(page) {
-  const containerId = "content-container";
+  const containerId = "content-container"
   
   // Load the corresponding HTML page based on the navigation
   switch(page) {
     case "characters":
-      loadPage("characters.html", containerId);
-      break;
+      loadPage("characters.html", containerId)
+      break
     case "comics":
-      loadPage("comics.html", containerId);
-      break;
+      loadPage("comics.html", containerId)
+      break
     case "games":
-      loadPage("games.html", containerId);
-      break;
+      loadPage("games.html", containerId)
+      break
     case "news":
-      loadPage("news.html", containerId);
-      break;
+      loadPage("news.html", containerId)
+      break
     default:
-      loadPage("index.html", containerId);
+      loadPage("index.html", containerId)
   }
 }
 
 // Function to handle navigation click events
 function handleNavigationClick(event) {
-  event.preventDefault();
-  const page = event.target.getAttribute("data-page");
-  navigateTo(page);
+  event.preventDefault()
+  const page = event.target.getAttribute("data-page")
+  navigateTo(page)
 }
 
 // Add click event listeners to navigation links
-const navigationLinks = document.getElementsByClassName("navigation-link");
+const navigationLinks = document.getElementsByClassName("navigation-link")
 for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", handleNavigationClick);
+  navigationLinks[i].addEventListener("click", handleNavigationClick)
 }
 
 // Initial page load (Home page)
-navigateTo("index.html");
-window.onload(navigateTo("index.html"))
+navigateTo("index.html")
